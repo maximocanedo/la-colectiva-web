926bdb9e2d47608b5536cd731ee8308cc7c78466
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2024-01-17 11:02:28 -0300

    Crear clase Usuario

--- src/data/models/user.ts
+++ src/data/models/user.ts
@@ -29,20 +29,29 @@ export interface IUser {
     active?: boolean;
 
 }
+export interface IUserEditRequest {
+    name?: string;
+    bio?: string;
+    email?: string;
+    birth?: Birth | null;
+}
 export interface IUserMethods {
     // Methods:
-    updateRole(role: Role): Promise<void>;
-    updatePassword(password: string): Promise<void>;
-    editProfile(data: any): Promise<void>;
-    disable(): Promise<void>;
-    // Statics: 
-    create(data: ISignUpRequest): Promise<CommonResponse>;
-    findById(id: string): Promise<IUser>;
-    findByEmail(email: string): Promise<IUser>;
-    myself(): Promise<IUser>;
-    usernameExists(username: string): Promise<boolean>;
+    isAdmin(): boolean;
+    isModerator(): boolean;
+    isNormal(): boolean;
+    isObserver(): boolean;
+    updateRole(role: Role): Promise<CommonResponse>;
+    updatePassword(password: string): Promise<CommonResponse>;
+    edit(data: IUserEditRequest): Promise<CommonResponse>;
+    disable(): Promise<CommonResponse>;
 }
 
+export interface MailVerificationSentResponse {
+    success: boolean;
+    validationId?: string;
+    error?: IError;
+}
 export class User implements IUser, IUserMethods {
     public _id: string;
     public name: string;
@@ -62,19 +71,119 @@ export class User implements IUser, IUserMethods {
         this.role = data.role;
         this.active = data.active;
     }
-    updateRole(role: Role): Promise<void> {
-        throw new Error("Method not implemented.");
+    /* Methods */
+    public isAdmin(): boolean {
+        return this.role === Role.ADMINISTRATOR;
     }
-    updatePassword(password: string): Promise<void> {
-        throw new Error("Method not implemented.");
+    public isModerator(): boolean {
+        return this.role === Role.MODERATOR;
     }
-    editProfile(data: any): Promise<void> {
-        throw new Error("Method not implemented.");
+    public isNormal(): boolean {
+        return this.role === Role.NORMAL;
     }
-    disable(): Promise<void> {
-        throw new Error("Method not implemented.");
+    public isObserver(): boolean {
+        return this.role === Role.OBSERVER;
     }
-    async create(data: ISignUpRequest): Promise<CommonResponse> {
+    
+    public async updateRole(role: Role): Promise<CommonResponse> {
+        const call = await u.patch(`users/${this.username}`, { role });
+        if(call != null) {
+            const { status } = call;
+            if(status === 200) return {
+                success: true,
+                message: "Rol actualizado correctamente. "
+            }; else {
+                const error: IError = (await call.json()).error;
+                return {
+                    success: false,
+                    ...error
+                };
+            }
+        }
+        return {
+            success: false,
+            message: "Error desconocido. "
+        };
+    }
+    public async updatePassword(password: string): Promise<CommonResponse> {
+        const call = await u.patch(`users/${this.username}`, { password });
+        if(call != null) {
+            const { status } = call;
+            if(status === 200) return {
+                success: true,
+                message: "Contraseña actualizada correctamente. "
+            }; else {
+                const error: IError = (await call.json()).error;
+                return {
+                    success: false,
+                    ...error
+                };
+            }
+        }
+        return {
+            success: false,
+            message: "Error desconocido. "
+        };        
+    }
+    public async edit(data: IUserEditRequest): Promise<CommonResponse> {
+        const call = await u.put(`users/${this.username}`, data);
+        if(call != null) {
+            const { status } = call;
+            if(status === 200) return {
+                success: true,
+                message: "Usuario editado correctamente. "
+            }; else {
+                const error: IError = (await call.json()).error;
+                return {
+                    success: false,
+                    ...error
+                };
+            }
+        }
+        return {
+            success: false,
+            message: "Error desconocido. "
+        };
+    }
+    public async disable(): Promise<CommonResponse> {
+        const call = await u.del(
+            `users/${this.username}`, 
+            {}
+        );
+        if(call != null) {
+            const { status } = call;
+            if(status === 200) return {
+                success: true,
+                message: "Usuario deshabilitado correctamente. "
+            }; else {
+                const error: IError = (await call.json()).error;
+                return {
+                    success: false,
+                    ...error
+                };
+            }
+        }
+        return {
+            success: false,
+            message: "Error desconocido. "
+        };
+    }
+    public async recover(): Promise<MailVerificationSentResponse> {
+        const call = await u.post(`users/${this.username}/recover`, {});
+        if(call !== null) {
+            const { status } = call;
+            const data = await call.json();
+            return {
+                success: status === 200,
+                ...data
+            };
+        }
+        return {
+            success: false
+        }
+    }
+    /* Statics */
+    public static async create(data: ISignUpRequest): Promise<CommonResponse> {
         const call = await u.post("users", data);
         if(call != null) {
             const { status } = call;
@@ -94,17 +203,138 @@ export class User implements IUser, IUserMethods {
             message: "Error desconocido. "
         };
     }
-    findById(id: string): Promise<IUser> {
-        throw new Error("Method not implemented.");
-    }
-    findByEmail(email: string): Promise<IUser> {
-        throw new Error("Method not implemented.");
-    }
-    myself(): Promise<IUser> {
-        throw new Error("Method not implemented.");
-    }
-    usernameExists(username: string): Promise<boolean> {
-        throw new Error("Method not implemented.");
-    }
+    public static async findById(id: string): Promise<User | IError> {
+        const call = await u.get(`users/${id}`);
+        if(call != null) {
+            const { status } = call;
+            if(status === 200) return new User(await call.json()); else {
+                const error: IError = (await call.json()).error;
+                return error;
+            }
+        }
+        return {
+            code: "unknown",
+            message: "Error desconocido. ",
+            details: "Error desconocido. "
+        };
 
+    }
+    public static async myself(): Promise<User | IError> {
+        const call = await u.get(`users/me`);
+        if(call != null) {
+            const { status } = call;
+            if(status === 200) return new User(await call.json()); else {
+                const error: IError = (await call.json()).error;
+                return error;
+            }
+        }
+        return {
+            code: "unknown",
+            message: "Error desconocido. ",
+            details: "Error desconocido. "
+        };
+    }
+    public static async usernameExists(username: string): Promise<boolean> {
+        const call = await u.head(`users/${username}`, {});
+        return call !== null && call.status === 200;
+    }
+    /* Actions on currently logged user */
+    public static async updateMail(mail: string): Promise<MailVerificationSentResponse> {
+        const call = await u.post("users/me/mail", { mail});
+        if(call != null) {
+            const { status } = call;
+            const data = await call.json();
+            return {
+                success: status === 200,
+                ...data
+            };
+        }
+        return {
+            success: false
+        }
+    }
+    public static async updatePassword(password: string): Promise<CommonResponse> {
+        const call = await u.patch(`users/me`, { password });
+        if(call != null) {
+            const { status } = call;
+            if(status === 200) return {
+                success: true,
+                message: "Contraseña actualizada correctamente. "
+            }; else {
+                const error: IError = (await call.json()).error;
+                return {
+                    success: false,
+                    ...error
+                };
+            }
+        }
+        return {
+            success: false,
+            message: "Error desconocido. "
+        };        
+    }
+    public static async tryCode(validationId: string, code: string, password?: string): Promise<CommonResponse> {
+        const call = await u.post(`users/validate/${validationId}`, { code, password });
+        if(call != null) {
+            const { status } = call;
+            if(status === 204) return {
+                success: true,
+                message: "Cuenta verificada/recuperada correctamente. "
+            }; else {
+                const error: IError = (await call.json()).error;
+                return {
+                    success: false,
+                    ...error
+                };
+            }
+        }
+        return {
+            success: false,
+            message: "Error desconocido. "
+        };        
+    }
+    public static async edit(data: IUserEditRequest): Promise<CommonResponse> {
+        const call = await u.put(`users/me`, data);
+        if(call != null) {
+            const { status } = call;
+            if(status === 200) return {
+                success: true,
+                message: "Tu cuenta fue editada correctamente. "
+            }; else {
+                const error: IError = (await call.json()).error;
+                return {
+                    success: false,
+                    ...error
+                };
+            }
+        }
+        return {
+            success: false,
+            message: "Error desconocido. "
+        };
+    }
+    public static async disable(): Promise<CommonResponse> {
+        const call = await u.del(
+            `users/me`, 
+            {}
+        );
+        if(call != null) {
+            const { status } = call;
+            if(status === 200) return {
+                success: true,
+                message: "Tu cuenta fue deshabilitada correctamente. "
+            }; else {
+                const error: IError = (await call.json()).error;
+                return {
+                    success: false,
+                    ...error
+                };
+            }
+        }
+        return {
+            success: false,
+            message: "Error desconocido. "
+        };
+    }
 }
+
