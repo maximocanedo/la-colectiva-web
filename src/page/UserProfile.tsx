db4722f0e1813f0be6c8ce7a7347d4198f7c04a3
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2024-02-11 12:32:14 -0300

    Crear componentes closes #9

---
+++ src/page/UserProfile.tsx
@@ -0,0 +1,19 @@
+import React, {ReactNode} from "react";
+import {useParams} from "react-router-dom";
+import UserButton from "../components/user/UserButton";
+const UserProfile = () => {
+    const username = useParams<{ username: string }>().username; // Obtiene el parámetro username de la URL
+
+    return (
+        <>
+            <UserButton
+                from={username as string}
+                onClick={
+                (e: React.MouseEvent<Element, MouseEvent>): void => {console.log({username, e});}
+                }
+            />
+        </>
+    );
+};
+
+export default UserProfile;
